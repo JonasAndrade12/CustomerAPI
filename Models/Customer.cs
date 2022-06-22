e85c6eaca66b8532616f8753dee8bd4ca886aff8
JonasAndrade12 <jonas_andrade@live.com.pt>
2022-06-22 23:59:25 +0100

    feat: add Customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+public class Customer
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public string FirstName { get; init; } = default!;
+
+    public string Surname { get; init; } = default!;
+
+    public string Email { get; init; } = default!;
+
+    public string Password { get; init; } = default!;
+}
